4bde30d0472a3ae5f5cc8df8fb2dc96428d9f9ac
Erza Kaneki <nevin.kadlec@gmail.com>
2024-09-24 17:37:30 -0500

    Created: project_1.sql project_1.db ran queries to make a short table called friends working through basic table functionality

--- Project_1/project_1.sql
+++ Project_1/project_1.sql
@@ -1,5 +1,38 @@
-CREATE TABLE friends (
-    id INTEGER,
-    name TEXT,
-    birthday INTEGER
-);
+-- CREATE TABLE friends (
+--     id INTEGER,
+--     name TEXT,
+--     birthday DATE
+-- );
+
+-- INSERT INTO friends (id, name, birthday)
+-- VALUES (1, 'Ororo Munroe', '1940-05-30');
+
+-- SELECT * FROM friends;
+
+-- INSERT INTO friends (id, name, birthday)
+-- VALUES (2, 'Mae Rae', '1993-08-30');
+
+-- INSERT INTO friends (id, name, birthday)
+-- VALUES (3, 'Mike Rae', '1978-08-31');
+
+-- UPDATE friends
+-- SET name = 'Storm'
+-- WHERE id = 1;
+
+-- ALTER TABLE friends
+-- ADD COLUMN email TEXT;
+
+-- UPDATE friends
+-- SET email = 'storm@codecademy.com'
+-- WHERE id = 1;
+
+-- UPDATE friends
+-- SET email = 'MaeRae@this.com'
+-- WHERE id = 2;
+
+-- UPDATE friends
+-- SET email = 'MikeRae@that.com'
+-- WHERE id = 3;
+
+-- DELETE FROM friends
+-- WHERE id = 1;
